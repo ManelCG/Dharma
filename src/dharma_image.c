ff16ea96db525c1881a9a63f2a71a0a5c259be70
MorktHrad <manelcg@protonmail.com>
2022-12-18 18:22:04 +0300

    Fixed overflow error

--- src/dharma_image.c
+++ src/dharma_image.c
@@ -141,7 +141,7 @@ bool dharma_image_set_pixel_from_1Barray(D_Image *im, uint32_t x, uint32_t y, co
 bool dharma_image_set_pixel_from_2Barray(D_Image *im, uint32_t x, uint32_t y, const uint16_t *array){
   uint32_t Bpp = im->bpp / 8;
 
-  memcpy(&im->data[(y*im->w + x) * Bpp], array, Bpp);
+  memcpy(&im->data[(y*im->w + x) * Bpp], array, Bpp / 2);
   return true;
 }
 
